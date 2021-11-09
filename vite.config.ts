5f8f2c8678362450fee0b3132e12467230f7fa13
r-kataoka11 <r.kataoka.ps.ai@gmail.com>
2021-11-09 13:52:30 +0900

    🔧 Viteをhttpsで起動するようにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import devcert from 'devcert'
+import { defineConfig, UserConfigExport } from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default async (): Promise<UserConfigExport> => {
+  const { key, cert } = await devcert.certificateFor('localhost')
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      open: true,
+      https: {
+        key,
+        cert,
+      },
+    },
+  })
+}
